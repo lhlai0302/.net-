5777498c0c73d89af78d8b49927bd196073bdbad
Tcyily <917125317@qq.com>
2019-10-22 23:16:20 +0800

    GradeEntry and GradeSearch(v1.0)

---
+++ Page/GradeModule/GradeEntry.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageTeacher.master" AutoEventWireup="true" CodeFile="GradeEntry.aspx.cs" Inherits="GradeEntry" Debug="true"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+<div class ="pannel pannel-default">
+        <div class =" pannel-heading">
+            <label>任课课程</label>
+            <%--<asp:SqlDataSource ID="ConnectionTest" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionTest %>" 
+                    SelectCommand="SELECT CourseName FROM [Course] ">
+            </asp:SqlDataSource>--%>
+            <%--<asp:DropDownList 
+                ID="ConnectionTestData" runat="server" AutoPostBack="true" DataSourceID ="ConnectionTest" DataTextField ="CourseName" OnSelectedIndexChanged="SelectCourse_OnChanged">
+            </asp:DropDownList>--%>
+             <asp:DropDownList 
+                ID="AllCourse" runat="server" AutoPostBack="true" OnSelectedIndexChanged="SelectCourse_OnChanged">
+            </asp:DropDownList>
+            <asp:GridView ID="CourseStudent" runat="server" 
+                OnSelectedIndexChanged="CourseStudent_SelectedIndexChanged" 
+                onRowEditing ="Edit" OnRowCancelingEdit="Cancel" OnRowUpdating  ="Update"
+                Width="377px" AutoGenerateColumns="False">
+                <Columns>
+                <asp:BoundField DataField="StudentID" HeaderText="学生ID" ReadOnly="True" />
+                <asp:BoundField DataField="StudentName" HeaderText="学生姓名" ReadOnly="True" />
+<%--                <asp:BoundField DataField="Score" HeaderText="分数" ReadOnly ="False" ApplyFormatInEditMode ="True"/>--%>
+                <asp:TemplateField HeaderText="分数" SortExpression="link">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="ScoreTextBox" runat="server" Width ="45px" Height ="20px" Text='<%# Eval("Score") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="ScoreLabel" runat="server" Text='<%# Eval("Score") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ShowEditButton="True" />
+                </Columns>
+                <HeaderStyle CssClass="center"/>
+                <RowStyle HorizontalAlign="Center" CssClass="perStyle"></RowStyle>
+            </asp:GridView>
+        </div>
+    </div>
+    
+</asp:Content>
---
+++ Page/GradeModule/GradeEntry.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Utility;
+public partial class GradeEntry : System.Web.UI.Page
+{
+    SqlHelper helper = new Utility.SqlHelper();
+    public static string connection = "Data Source=DESKTOP-SS8O6R6\\SQLEXPRESS;Initial Catalog=StudentSystem; User id = sa; Password = 917125317";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack) return;
+        //TODO: 从session获取
+        int teacherID = 2;
+
+        string sqlQuery = "SELECT * FROM Course where TeacherID = @teacherID";
+        DataTable dt = helper.ExecuteDataTable(sqlQuery, CommandType.Text, new SqlParameter[] { new SqlParameter("@teacherID", teacherID) });
+
+        Hashtable ht = new Hashtable();
+        ht.Add("NULL", null);
+        foreach(DataRow dr in dt.Rows)
+        {
+            ht.Add(dr["CourseID"], dr["CourseName"]);
+        }
+        AllCourse.DataSource = ht;
+        AllCourse.DataValueField = "key";
+        AllCourse.DataTextField = "value";
+        AllCourse.DataBind();
+    }
+
+    protected void SelectCourse_OnChanged(object sender, EventArgs e)
+    {
+        GetData();
+    }
+
+    protected void CourseStudent_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void Edit(object sender, GridViewEditEventArgs e)
+    {
+        CourseStudent.EditIndex = e.NewEditIndex;
+        GetData();
+    }
+    protected void Cancel(object sender, GridViewCancelEditEventArgs e)
+    {
+        CourseStudent.EditIndex = -1;
+        GetData();
+    }
+    protected void Update(object sender, GridViewUpdateEventArgs e)
+    {
+        string studentIDStr = CourseStudent.Rows[e.RowIndex].Cells[0].Text;
+        string scoreStr = ((TextBox)CourseStudent.Rows[e.RowIndex].FindControl("ScoreTextBox")).Text;
+        
+        int studentID = int.Parse(studentIDStr);
+        int score = int.Parse(scoreStr);
+        string updateSql = "Update StudentCourse Set Score = @score Where StudentID = @studentID and CourseID = @courseID";
+        helper.ExecuteNonQuery(updateSql, CommandType.Text, new SqlParameter[] { new SqlParameter("@score", score),
+                                                     new SqlParameter("@studentID", studentID), new SqlParameter("@courseID", AllCourse.SelectedItem.Value)});
+        CourseStudent.EditIndex = -1;
+        GetData();
+    }
+
+    protected void GetData()
+    {
+        //SqlConnection con = new SqlConnection(connection);
+        //con.Open();
+        //SqlCommand comm = new SqlCommand(sqlQuery, con);
+        //comm.Parameters.AddWithValue("@courseID", AllCourse.SelectedItem.Value);
+        //SqlDataAdapter adapter = new SqlDataAdapter();
+        //adapter.SelectCommand = comm;
+        //DataSet dataSet = new DataSet();
+        //adapter.Fill(dataSet);
+        //con.Close();
+
+        string sqlQuery = "SELECT * FROM StudentCourse inner join Student on StudentCourse.CourseID = @courseID and StudentCourse.StudentID = Student.StudentID";
+        try
+        {
+            DataTable dt = helper.ExecuteDataTable(sqlQuery, CommandType.Text, new SqlParameter[] { new SqlParameter("@courseID", AllCourse.SelectedItem.Value) });
+            CourseStudent.DataSource = dt;
+            CourseStudent.DataBind();
+        }
+        catch(Exception e)
+        {
+            DataTable dt = null;
+            CourseStudent.DataSource = dt;
+            CourseStudent.DataBind();
+        }
+        
+    }
+}
